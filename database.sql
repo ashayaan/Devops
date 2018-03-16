819d67d78042e690968adf602fb82173577ca9a0
ashayaan <a.shayaan@gmail.com>
2018-03-16 13:19:24 +0530

    sql file

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-drop database IF EXISTS devops;
-create database devops;
+drop database IF EXISTS shayaan;
+create database shayaan;
 use shayaan;
 CREATE TABLE users(ID int NOT NULL AUTO_INCREMENT,Name varchar(255) NOT NULL, username varchar(255),password varchar(45),email varchar(45),PRIMARY KEY (ID) );
